51f1f7825db41ef5095be8f86d23d219bc4e3abc
Kusioo21 <kusio360@gmail.com>
2025-11-19 02:28:06 +0100

    zadanie 1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        int[] liczby = { 5, 10, 15, 20, 25 };
+
+        int suma = 0;
+
+        for (int i = 0; i < liczby.Length; i++)
+        {
+            suma += liczby[i];
+        }
+
+        Console.WriteLine("Suma elementów tablicy wynosi: " + suma);
+    }
+}
