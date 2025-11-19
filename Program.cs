80704adfb434d03b3769be81bd64dd615e8e4721
Kusioo21 <kusio360@gmail.com>
2025-11-19 02:33:10 +0100

    zadanie 2

--- Program.cs
+++ Program.cs
@@ -4,16 +4,19 @@ class Program
 {
     static void Main()
     {
+        int[] liczby = { 12, 7, 45, -3, 22 };
+        int maks = liczby[0];
+        int min = liczby[0];
 
-        int[] liczby = { 5, 10, 15, 20, 25 };
-
-        int suma = 0;
-
-        for (int i = 0; i < liczby.Length; i++)
+        for (int i = 1; i < liczby.Length; i++)
         {
-            suma += liczby[i];
+            if (liczby[i] > maks)
+                maks = liczby[i];
+            if (liczby[i] < min)
+                min = liczby[i];
         }
 
-        Console.WriteLine("Suma elementów tablicy wynosi: " + suma);
+        Console.WriteLine("Największy element: " + maks);
+        Console.WriteLine("Najmniejszy element: " + min);
     }
 }
